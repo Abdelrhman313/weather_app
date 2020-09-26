f40dd3f6a7f1c0ca35e87eaf4659df613011b618
Abdelrhman <58594875+Abdelrhman313@users.noreply.github.com>
2020-09-27 00:23:03 +0200

    add files

---
+++ build/precache-manifest.6fada7c5848e1c64cbf03ecf59c6d920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9236cd0e06bf550c037d1edd0c68701",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae31300956f5e7ece45c",
+    "url": "/static/css/main.acf3b861.chunk.css"
+  },
+  {
+    "revision": "49acb864f6bb32bd5863",
+    "url": "/static/js/2.e5e02872.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5e02872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae31300956f5e7ece45c",
+    "url": "/static/js/main.f9d16e1d.chunk.js"
+  },
+  {
+    "revision": "3b2f54f398e51c6343ab",
+    "url": "/static/js/runtime-main.de52cee1.js"
+  }
+]);
